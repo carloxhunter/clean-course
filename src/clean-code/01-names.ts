4ef0e64a99bf2e1fe45b404a3dcda14e236b0065
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-06-03 13:14:17 -0400

    ej1: answer

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -1,18 +1,17 @@
+import fs from 'fs';
+
 (() => {
 
-  // Ejemplo
-  // Archivos a evaluar - files to evaluate
-  const fs = [
-    { id: 1, f: false },
-    { id: 2, f: false },
-    { id: 3, f: true },
-    { id: 4, f: false },
-    { id: 5, f: false },
-    { id: 7, f: true },
+  const files = [
+    { id: 1, toDelete: false },
+    { id: 2, toDelete: false },
+    { id: 3, toDelete: true },
+    { id: 4, toDelete: false },
+    { id: 5, toDelete: false },
+    { id: 7, toDelete: true },
   ];
 
-  // Archivos marcados para borrar - files to delete
-  const arhivos = fs.map(f => f.f);
+  const filesToDelete = files.map(file => file.toDelete);
 
 
   class AbstractUser { };
@@ -28,25 +27,28 @@
   // Todo: Tarea
 
   // día de hoy - today
-  const ddmmyyyy = new Date();
+  const today = new Date();
 
   // días transcurridos - elapsed time in days
-  const d: number = 23;
+  const daysSince: number = 23;
+  // R: const elapsedTimeInDays: number = 23;
 
   // número de archivos en un directorio - number of files in directory
-  const dir = 33;
+  const numberOfFilesInDirectory = 33;
 
   // primer nombre - first name
-  const nombre = 'Fernando';
+  const firstName = 'Fernando';
 
   // primer apellido - last name
-  const apellido = 'Herrera';
+  const latName = 'Herrera';
 
   // días desde la última modificación - days since modification
-  const dsm = 12;
+  const daysSinceLastUpdate = 12;
+ // R:  const daysSinceLastModification = 12;
 
   // cantidad máxima de clases por estudiante - max classes per student
-  const max = 6;
+  const studentMaximunAmountOfClasses = 6;
+  // R: const macClassesPerStudent = 6;
 
 
 })();
