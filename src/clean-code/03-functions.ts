9142ed4ae2fd810d55cea9afcc991806a8ebf762
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-06-05 12:28:09 -0400

    ej3: listo

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,27 +1,30 @@
 (() => {
 
-  // función para obtener información de una película por Id
-  function getAllMovies(movieId: string) {
+  function getMovieById(movieId: string) {
     console.log({ movieId });
   }
 
-  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors(id: string) {
-    console.log({ id });
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
   }
 
-  // funcion para obtener el bio del actor por el id
-  function getUsuario(ActorId: string) {
-    console.log({ ActorId });
+  function getActorBioById(actorId: string) {
+    console.log({ actorId});
   }
 
-  // Crear una película
-  function movie(title: string, description: string, rating: number, cast: string[]) {
+  interface createMovieParams {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+  // R: interfaz Movie (y ordenadas ascendente)
+
+  function createMovie({title, description, rating, cast} : createMovieParams)  {
     console.log({ title, description, rating, cast });
   }
 
-  // Crea un nuevo actor
-  function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+  function createActor(fullName: string, birthDate: Date): boolean {
 
     // tarea asincrona para verificar nombre
     // ..
