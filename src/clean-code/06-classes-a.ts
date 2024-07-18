672e4a9cefed84b9861bdac9d86cd48a0638c3f9
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-07-18 10:55:17 -0400

    feat: Add User and UserSettings classes with inheritance and additional properties

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -25,4 +25,48 @@
   const newPerson2 = new Person2('Charly', 'M', new Date('1994-07-25'));
   console.log({ newPerson2 });
 
+  class User extends Person2 {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+        super( name, gender, birthdate);
+        this.lastAccess = new Date;
+      }
+      checkCredentials(): boolean {
+        return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date,
+    ) {
+        super(email, role, name, gender, birthdate);
+    }}
+
+
+    const userSettings = new UserSettings(
+      '/user/charly',
+      'home',
+      'charly@gmail.com',
+      'Admin',
+      'Charly',
+      'M',
+      new Date('1994-07-25')
+    )
+     
+    console.log({ userSettings, areCredentialsOk: userSettings.checkCredentials() });
+
 })();
