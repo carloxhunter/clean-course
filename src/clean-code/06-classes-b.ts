eb52a91d4b0fe05ba286939c8dd40f4c616ac1d7
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-08-22 17:12:53 -0400

    Refactor Person and User classes with constructor parameter destructuring

--- src/clean-code/06-classes-b.ts
+++ src/clean-code/06-classes-b.ts
@@ -1,73 +1,86 @@
 (() => {
+  // no aplicando principio de responsabilidad única
+  type Gender = 'M' | 'F';
 
-    type Gender = 'M' | 'F';
-    
-    class Person1 {
-      public name: string;
-      public gender: Gender;
-      public birthdate: Date;
-      constructor(name: string, gender: Gender, birthdate: Date) {
-        this.name = name;
-        this.gender = gender;
-        this.birthdate = birthdate
+  interface PersonProps {
+    name: string
+    gender: Gender
+    birthdate: Date
+  }
+
+  class Person {
+    public name: string;
+    public gender: Gender;
+    public birthdate: Date;
+    constructor({ name, gender, birthdate }: PersonProps) {
+      this.name = name;
+      this.gender = gender;
+      this.birthdate = birthdate
     }
-    
-  }
-    const newPerson1 = new Person1('Charly', 'M', new Date('1994-07-25')); 
-    console.log({ newPerson1 });
 
-    class Person2 { 
-      constructor(
-        public name: string,
-        public gender: Gender,
-        public birthdate: Date
-    ){}
   }
-  const newPerson2 = new Person2('Charly', 'M', new Date('1994-07-25'));
-  console.log({ newPerson2 });
+  const newPerson1 = new Person({ name: 'Charly', gender: 'M', birthdate: new Date('1994-07-25') });
+  console.log({ newPerson1 });
 
-  class User extends Person2 {
+
+  interface UserProps extends PersonProps {
+    email: string;
+    role: string;
+  }
+
+  class User extends Person {
     public lastAccess: Date;
+    public email: string;
+    public role: string;
 
     constructor(
-      public email: string,
-      public role: string,
-      name: string,
-      gender: Gender,
-      birthdate: Date
+      { email,
+        role,
+        name,
+        gender,
+        birthdate }: UserProps
     ) {
-        super( name, gender, birthdate);
-        this.lastAccess = new Date;
-      }
-      checkCredentials(): boolean {
-        return true;
+      super({ name, gender, birthdate });
+      this.lastAccess = new Date;
+      this.email = email;
+      this.role = role;
     }
+    checkCredentials(): boolean {
+      return true;
+    }
+  }
+
+  interface UserSettingsProps extends UserProps {
+    workingDirectory: string;
+    lastOpenFolder: string;
   }
 
   class UserSettings extends User {
-    constructor(
-      public workingDirectory: string,
-      public lastOpenFolder: string,
-      email: string,
-      role: string,
-      name: string,
-      gender: Gender,
-      birthdate: Date,
-    ) {
-        super(email, role, name, gender, birthdate);
-    }}
+    public workingDirectory: string;
+    public lastOpenFolder: string;
+
+    constructor({ email, role, name, gender, birthdate, workingDirectory, lastOpenFolder }: UserSettingsProps) {
+      {
+        super({ email, role, name, gender, birthdate });
+        this.workingDirectory = workingDirectory;
+        this.lastOpenFolder = lastOpenFolder;
+      }
+    }
+  }
 
 
-    const userSettings = new UserSettings(
-      '/user/charly',
-      'home',
-      'charly@gmail.com',
-      'Admin',
-      'Charly',
-      'M',
-      new Date('1994-07-25')
-    )
-     
-    console.log({ userSettings, areCredentialsOk: userSettings.checkCredentials() });
+  const userSettings = new UserSettings({
+    workingDirectory: '/user/charly',
+    lastOpenFolder: 'home',
+    email: 'charly@gmail.com',
+    role: 'Admin',
+    name: 'Charly',
+    gender: 'M',
+    birthdate: new Date('1994-07-25')
+  }
+  )
+
+
+  console.log({ userSettings, areCredentialsOk: userSettings.checkCredentials() });
 
 })();
