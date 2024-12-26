77b4ffd6fea5bf40cdd5e2c1fe356bd5dbc959bd
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-12-26 12:40:27 -0300

    feat: Replace axios with fetch in HttpClient and remove axios dependency

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,11 +1,8 @@
-import axios from 'axios';
-
 export class HttpClient {
   async get(url: string) {
-    const { data, status } = await axios.get(url);
-    console.log({ status });
-    return data;
+    const response = await fetch(url);
+    const data = await response.json();
+   
+    return { data, status: response.status };
   }
-
-
 }
