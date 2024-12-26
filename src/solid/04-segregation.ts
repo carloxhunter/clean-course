0a188473d84300d55ec8c316577942e66ef7cec8
Carlos Elgueta <carlos.elgueta@bicevida.cl>
2024-12-26 14:11:56 -0300

    feat: Implement Bird interface and classes for Tucan, Hummingbird, Ostrich, and Penguin

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,38 @@
+interface Bird {
+  fly(): void;
+  eat(): void;
+  run(): void;
+  swim(): void;
+}
+
+class Tucan implements Bird {
+
+  public fly(): void {}
+  public eat(): void {}
+  public run(): void {}
+  public swim(): void {}
+}
+
+class Hummingbird implements Bird {
+
+  public fly(): void {}
+  public eat(): void {}
+  public run(): void {}
+  public swim(): void { }
+}
+
+class Ostich implements Bird {
+
+  public fly(): void {}
+  public eat(): void {}
+  public run(): void {}
+  public swim(): void { }
+}
+
+class Penguin implements Bird {
+
+  public fly(): void {}
+  public eat(): void {}
+  public run(): void {}
+  public swim(): void {}
+}
